a758e7ae5f133b8c49a5bae756cad47042cf5172
Atharva <Atharvapawar34s@gmail.com>
2024-02-24 13:21:27 +0530

    update folder to GitHub

--- petstoreapp/forms.py
+++ petstoreapp/forms.py
@@ -4,11 +4,10 @@ from .models import Pet
 class PetForm(forms.ModelForm):
     class Meta:
         model = Pet
-        fields =  ['name', 'breed', 'price', 'image']
+        fields = ['name', 'breed', 'price', 'image']
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'breed': forms.TextInput(attrs={'class': 'form-control'}),
             'price': forms.NumberInput(attrs={'class': 'form-control'}),
             'image': forms.FileInput(attrs={'class': 'form-control-file'}),
-
         }
